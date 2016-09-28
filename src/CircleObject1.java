96f36b48b68722c6d310de1e3f0016f59a124c8b
Yasmine Lee <yaslee3@yahoo.com>
2016-09-28 10:51:00 -0400

    initial commit added code

--- src/CircleObject1.java
+++ src/CircleObject1.java
@@ -5,49 +5,43 @@ public class CircleObject1 {
 	private static int circlecount;
 	private double radius;
 
-	public CircleObject1(double radius) {
+	public CircleObject1(double radius) {//constructor
 		this.radius = radius;
 				CircleObject1.circlecount++;
 	}
-	//Get Circumference
+// get Circumference
+	public double getCircumference() {
 	
+		return (2* Math.PI * radius) ;
 		
+		
+	}
+	//Get Formatted Circumference
+public String getFormattedCircumference () {
+	return formatNumber (getCircumference());
+}
 	
-	//Get FormattedCircumference
+private String formatNumber(double x) {
 	
-	
+	return new DecimalFormat ("##.##").format (x);
+}
+// Get Area
+public double getArea () {
+	return (Math.PI * radius * radius);
+}
 
-//Get Area 
-
-
-	
-//Get FormattedArea
-
-
-//Get object count 
-	
-
+	//Get Formatted Area
+	public String getFormattedArea() {
+		
+		return formatNumber(getArea());
 	}
 
+	// Get Object Count 
+	public static int getObjectCount() {
+		
+		return CircleObject1.circlecount;
+	}
 
-
-
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-
+	}
 
 
